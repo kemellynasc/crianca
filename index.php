020ec5a32e17e4bcea9c1c5df820a4997573f9ac
kemelly <kemellynasc@gmail.com>
2020-10-15 14:36:42 -0300

    ccHIDROCRI

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+ <link rel="stylesheet" href="css/css.css">
+    
     <title>Document</title>
 </head>
 <body>
     <h1>criança</h1>
-<<<<<<< HEAD
 
-
-=======
-    <h2>adolescente</h2>
->>>>>>> 95da94793a5f120aa5415cc9a1cb4c185ed56f8f
+    <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">hidro </a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+  </form>
+</nav>
     
+    
+
+
+
 </body>
 </html>
